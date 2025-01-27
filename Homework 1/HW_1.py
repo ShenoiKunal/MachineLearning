1b59246f3c47d08841bf010b7fae444751e4bdf9
ShenoiKunal <panamakunal@gmail.com>
2025-01-27 18:50:07 -0500

    Stratified Shuffle Split for set based on income category

--- Homework 1/HW_1.py
+++ Homework 1/HW_1.py
@@ -3,35 +3,40 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from zlib import crc32
-
-
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 
 # Load data into PD dataframe
 housing = pd.read_csv(Path("datasets/housing.csv"))
 
-# Shuffle dataset, take the first x% determined by ratio, and split into test and train sets.
-def shuffle_and_split_data(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
 
 # Check if 32-bit checksum is in the within the test ratio set (ratio * 2^32)
 def is_id_in_test_set(identifier, test_ratio):
     return crc32(np.int64(identifier)) < test_ratio * 2**32
 
-# Split data based on hashed ID
-def split_data_with_id_hash(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
+# Show that the random samples are always the same with the same random_state
+#     trainSplit, testSplit = train_test_split(housing, test_size=0.2, random_state=42)
+#     print("First Random Training Sample (Rows 1-5)")
+#     print(trainSplit[1:5])
+#     print("First Random Test Sample (Rows 1-5)")
+#     print(testSplit[1:5])
+#     second_trainSplit, second_testSplit = train_test_split(housing, test_size=0.2, random_state=42)
+#     print("Second Random Training Sample (Rows 1-5)")
+#     print(second_trainSplit[1:5])
+#     print("Second Random Test Sample (Rows 1-5)")
+#     print(second_testSplit[1:5])
+
 
 # Cut set into bins based on median_income
 housing["income_cat"] = pd.cut(housing["median_income"], bins=[0,1.5,3.0,4.5,6.0,np.inf], labels=[1,2,3,4,5])
 
-# Create bar graph
-housing["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
-plt.xlabel("Income Category")
-plt.ylabel("Number of districts")
-plt.show()
+# Split set by median_income category
+splitter = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
+strat_splits = []
+for train_index, test_index in splitter.split(housing, housing["income_cat"]):
+    strat_train_set_n = housing.iloc[train_index]
+    strat_test_set_n = housing.iloc[test_index]
+    strat_splits.append([strat_train_set_n,strat_test_set_n])
+strat_train_set, strat_test_set = strat_splits[0]
+
+print(strat_test_set.describe())
+print(strat_train_set.describe()) 
