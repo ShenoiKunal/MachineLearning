bf526c1311849470a785b77b8b361cd09818d25e
ShenoiKunal <panamakunal@gmail.com>
2025-01-27 17:29:52 -0500

    Set of descriptions and info

--- Homework 1/HW_1.py
+++ Homework 1/HW_1.py
@@ -1,8 +1,14 @@
 from dataclasses import asdict
 from pathlib import Path
+from pydoc import describe
+
 import pandas as pd
 
 # Load data into PD dataframe
 housing = pd.read_csv(Path("datasets/housing.csv"))
 
-print(housing.head())
+print("\nHEAD\n--------\n",housing.head())
+print("\nInfo\n--------")
+print(housing.info())
+print("\nOcean Proximity\n--------\n",housing["ocean_proximity"].value_counts())
+print("\nDescribe\n--------\n",housing.describe())
